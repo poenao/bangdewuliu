34efd8509fb8c19ade176317b153795aab06db13
poenao <940837938@qq.com>
2026-04-06 04:40:20 +0800

    请求拦截器401返回登录页把上一页地址拼接然后在登录页拿到判断是否跳转判断数组中是否包含当前页面的路由路径，如果包含则说明当前页面是 tabbar 页面，否则不是跳转到原来的页面

--- apis/uni-fetch.js
+++ apis/uni-fetch.js
@@ -2,6 +2,12 @@
 import { createUniFetch } from 'uni-app-fetch'
 import { useUserStore } from '../stores/user'
 
+// 将当前所有tabbar页面的路径放在一个数组中，方便后续判断
+const tabbarPages = [
+  'pages/task/index',
+  'pages/message/index',
+  'pages/my/index',
+]
 // 配置符合自身业务的请求对象
 export const fetch = createUniFetch({
   loading: { title: '正在加载...' },
@@ -23,7 +29,19 @@ export const fetch = createUniFetch({
       if (statusCode === 401) {
         // 401 状态码表示未授权，可能是 token 无效或过期
         // 跳转登录页
-        uni.redirectTo({ url: '/pages/login/index' })
+        // 获取页面栈
+        const pageStack = getCurrentPages()
+        // 获取上一页 取的页面栈数组最后一个元素
+        const currentPage = pageStack[pageStack.length - 1]
+        const redirectURL = currentPage.route // 获取当前页面的路由路径
+        // 判断数组中是否包含当前页面的路由路径，如果包含则说明当前页面是 tabbar 页面，否则不是
+        const routeType = tabbarPages.includes(redirectURL)
+          ? 'switchTab'
+          : 'redirectTo'
+        // 跳转到登录页，并将当前页面的路由路径作为参数传递，登录成功后可以根据这个参数跳转回原页面
+        uni.redirectTo({
+          url: `/pages/login/index?redirectURL=${redirectURL}&routeType=${routeType}`,
+        })
       }
       return data
     },
--- pages/login/components/account.vue
+++ pages/login/components/account.vue
@@ -32,6 +32,25 @@
   import { loginAPI } from '../../../apis/user'
   import { useUserStore } from '../../../stores/user'
 
+  // //生命周期获取页面参数
+  // onLoad((query) => {
+  //   // 获取待跳转的页面路径
+  //   redirectURL.value = query.redirectURL
+  //   // 获取页面跳转方式
+  //   routeType.value = query.routeType || 'navigateTo' // 默认使用 navigateTo 路由方式
+  // })
+
+  // ✅ 用 defineProps 接收父组件传来的参数
+  const props = defineProps({
+    redirectURL: {
+      type: String,
+      default: 'pages/task/index',
+    },
+    routeType: {
+      type: String,
+      default: 'switchTab',
+    },
+  })
   const form = ref(null)
 
   // 表单数据
@@ -39,6 +58,10 @@
     account: '',
     password: '',
   })
+  // 待跳转的页面路径
+  const redirectURL = ref('')
+  // 页面跳转方式
+  const routeType = ref('') // 默认使用 navigateTo 路由方式
   // 定义表单数据验证规则
   const accountRules = reactive({
     account: {
@@ -61,11 +84,16 @@
       await form.value.validate()
       // 校验通过，执行登录逻辑
       const res = await loginAPI(formData)
-      console.log(res.data.code)
+      console.log(res.code)
       // 检测接口是否调用成功
-      if (res.data.code !== 200) return uni.utils.toast('登录失败，请重试！')
+      if (res.code !== 200) return uni.utils.toast('登录失败，请重试！')
       const store = useUserStore()
-      store.token = res.data.data
+      store.token = res.data
+      // 当登录成功后需要跳转原来的页面
+      // 根据父组件传来的参数进行页面跳转
+      uni[props.routeType]({ url: `/${props.redirectURL}` })
+      //提示登录成功
+      uni.utils.toast('登录成功！')
     } catch (error) {
       // 校验失败，错误信息会在表单项下方显示
       console.error('登录失败', error)
