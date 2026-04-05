75a6aba5c90c335afd1b01a8ff3b7f136ddb48fb
poenao <940837938@qq.com>
2026-04-06 02:30:34 +0800

    在请求拦截器里设置请求头注入token然后相应拦截器里过滤多余数据

--- apis/uni-fetch.js
+++ apis/uni-fetch.js
@@ -1,5 +1,6 @@
 // 导入安装好的 uni-app-fetch 模块
 import { createUniFetch } from 'uni-app-fetch'
+import { useUserStore } from '../stores/user'
 
 // 配置符合自身业务的请求对象
 export const fetch = createUniFetch({
@@ -8,13 +9,26 @@ export const fetch = createUniFetch({
   intercept: {
     // 请求拦截器
     request(options) {
-      // 后续补充实际逻辑
+      // 1. 获取 Pinia 中的用户 store
+      const userState = useUserStore()
+
+      // 2. 定义默认请求头，携带 token
+      const defaultHeaders = {
+        Authorization: userState.token,
+      }
+
+      // 3. 合并请求头
+      options.header = Object.assign({}, defaultHeaders, options.header)
+
+      // 4. 返回修改后的配置
       return options
     },
     // 响应拦截器
-    response(result) {
+    response({ data }) {
       // 后续补充实际逻辑
-      return result
+      // 过滤多余数据，直接返回接口需要的数据
+
+      return data
     },
   },
 })
