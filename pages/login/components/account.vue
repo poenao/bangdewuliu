230ea106c2d9462ad8930cb7abf0d7f5c8d1b40d
poenao <940837938@qq.com>
2026-04-06 01:43:35 +0800

    登录页面-账号登录准备基本模板和数据双向绑定还有校验

--- pages/login/components/account.vue
+++ pages/login/components/account.vue
@@ -1,27 +1,72 @@
 <template>
-	<uni-forms class="login-form" ref="form">
-		<uni-forms-item name="name">
-			<input
-				type="text"
-				placeholder="请输入账号"
-				class="uni-input-input"
-				placeholder-style="color: #818181"
-			/>
-		</uni-forms-item>
-		<uni-forms-item name="name">
-			<input
-				type="text"
-				placeholder="请输入密码"
-				class="uni-input-input"
-				placeholder-style="color: #818181"
-			/>
-		</uni-forms-item>
-		<button class="submit-button">登录</button>
-	</uni-forms>
+  <uni-forms
+    class="login-form"
+    ref="form"
+    :model="formData"
+    :rules="accountRules"
+    validateTrigger="bind"
+  >
+    <uni-forms-item name="account">
+      <uni-easyinput
+        type="text"
+        placeholder="请输入账号"
+        class="uni-input-input"
+        placeholder-style="color: #818181"
+        v-model="formData.account"
+      />
+    </uni-forms-item>
+    <uni-forms-item name="password">
+      <uni-easyinput
+        type="password"
+        placeholder="请输入密码"
+        class="uni-input-input"
+        placeholder-style="color: #818181"
+        v-model="formData.password"
+      />
+    </uni-forms-item>
+    <button class="submit-button" @click="onSubmit">登录</button>
+  </uni-forms>
 </template>
 
-<script></script>
+<script setup>
+  import { ref, reactive } from 'vue'
+
+  const form = ref(null)
+
+  // 表单数据
+  const formData = reactive({
+    account: '',
+    password: '',
+  })
+  // 定义表单数据验证规则
+  const accountRules = reactive({
+    account: {
+      rules: [
+        { required: true, errorMessage: '请输入登录账号' },
+        { pattern: '^[a-zA-Z0-9]{6,8}$', errorMessage: '登录账号格式不正确' },
+      ],
+    },
+    password: {
+      rules: [
+        { required: true, errorMessage: '请输入登录密码' },
+        { pattern: '^\\d{6}$', errorMessage: '登录密码格式不正确' },
+      ],
+    },
+  })
+
+  // 提交校验
+  const onSubmit = () => {
+    form.value
+      .validate()
+      .then((res) => {
+        console.log('校验通过：', res)
+      })
+      .catch((err) => {
+        console.log('校验失败：', err)
+      })
+  }
+</script>
 
 <style lang="scss" scoped>
-	@import './styles.scss';
+  @import './styles.scss';
 </style>
