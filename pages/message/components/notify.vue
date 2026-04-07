8f3bd5ee4454c8c619cc698dfa7a4a0f5eab0dfe
poenao <940837938@qq.com>
2026-04-07 11:02:30 +0800

    监听滚动数据滚动到底部的时候触发下一页数据

--- pages/message/components/notify.vue
+++ pages/message/components/notify.vue
@@ -8,17 +8,39 @@
   const page = ref(1)
   // 请求每页条数
   const pageSize = ref(10)
-
-  const getNotifyList = async () => {
-    const res = await messageListAPI(201, page.value, pageSize.value)
-    notifyList.value = res.data.items
+  // 是否为空列表
+  const isEmpty = ref(false)
+  // 记录每次滚动页码
+  const scrollPage = ref(1)
+  const getNotifyList = async (page = 1, pageSize = 10) => {
+    const res = await messageListAPI(201, page, pageSize)
+    console.log(res)
+    if (res.code !== 200) return uni.utils.toast('获取消息列表失败')
+    // 更新任务列表数据
+    notifyList.value.push(...res.data.items)
+    // 是否为空列表
+    isEmpty.value = notifyList.value.length === 0
+    // 记录每次滚动到底部的页码
+    scrollPage.value = page.value
+  }
+  // 监听用户是否滚动到页码底部
+  const onScrollToLower = () => {
+    // 如果是空列表则不请求下一页数据
+    if (isEmpty.value) return
+    // 请求下一页数据
+    getNotifyList(scrollPage.value, pageSize.value)
   }
   onMounted(() => {
     getNotifyList()
   })
 </script>
 <template>
-  <scroll-view class="scroll-view" refresher-enabled scroll-y>
+  <scroll-view
+    class="scroll-view"
+    refresher-enabled
+    scroll-y
+    @scrolltolower="onScrollToLower"
+  >
     <view class="scroll-view-wrapper">
       <view class="message-action">
         <text class="iconfont icon-clear"></text>
